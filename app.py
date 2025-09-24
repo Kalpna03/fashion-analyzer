8bba23a9458cc56cbe5ef53b2c4134b9b2457d45
Kalpna03 <139703730+Kalpna03@users.noreply.github.com>
2025-09-24 14:18:58 +0530

    Update app.py with correct url

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ elif page == "📉 Trend Analysis":
     progress_bar.empty()
     with st.spinner("Finalizing dashboard..."):
         time.sleep(0.3)
-        looker_studio_url_3 = "https://lookerstudio.google.com/embed/reporting/66dbd436-1ac5-46fa-9a67-b4384db6a2d6/page/JrlWF"
+        looker_studio_url_3 = "https://lookerstudio.google.com/embed/reporting/12e0f067-f32d-43f9-bf6d-e04e82ffaac8/page/p_c5tagt8dwd"
 
         # --- LOGIC CORRECTED: This will now display the dashboard directly ---
         st.components.v1.iframe(looker_studio_url_3, height=550, scrolling=True)
@@ -127,6 +127,7 @@ elif page == "ℹ️ About":
 
 st.divider()
 st.caption("© 2025 Agent Analytics Hub | Powered by Streamlit & Google Cloud")
+
 
 
 
